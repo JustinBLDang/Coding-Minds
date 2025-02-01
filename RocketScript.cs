e98bbf086f90631eb4000039b2fe2802a5635da2
JustinBLDang <57662868+JustinBLDang@users.noreply.github.com>
2025-01-31 19:33:11 -0800

    Update RocketScript.cs

--- RocketScript.cs
+++ RocketScript.cs
@@ -17,23 +17,18 @@ public class RocketScript : MonoBehaviour
     void FixedUpdate()
     {
         if(Input.GetKey("space")){
-            Debug.Log("body");
             rb.AddForce(transform.up.normalized * mainThrust, ForceMode.Force);
         }
         if(Input.GetKey("up")){
-            Debug.Log("f");
             rb.AddForceAtPosition(transform.up.normalized  * controlThrust, fThrust.transform.position, ForceMode.Force);
         }
         if(Input.GetKey("down")){
-            Debug.Log("b");
             rb.AddForceAtPosition(transform.up.normalized  * controlThrust, bThrust.transform.position, ForceMode.Force);
         }
         if(Input.GetKey("right")){
-            Debug.Log("r");
             rb.AddForceAtPosition(transform.up.normalized  * controlThrust, rThrust.transform.position, ForceMode.Force);
         }
         if(Input.GetKey("left")){
-            Debug.Log("l");
             rb.AddForceAtPosition(transform.up.normalized  * controlThrust, lThrust.transform.position, ForceMode.Force);
         }
     }
